d64215d967c93e9ebc4b5e655e060ea147245a71
Eric Mascot <eric.mascot@donuts.ne.jp>
2014-03-29 19:32:03 +0700

    Initial commit

---
+++ ExperienceBar.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExperienceBar : MonoBehaviour
+{
+	[Range(0,1)]
+	public float
+		percent;
+	float oldPercent = 0;
+	Material material;
+
+	void Start ()
+	{
+		material = renderer.material;
+	}
+
+	void Update ()
+	{
+		if (oldPercent != percent) {
+			material.SetTextureOffset ("_MaskTex", Vector2.right * (1 - percent * 2));
+			oldPercent = percent;
+		}
+	}
+}
